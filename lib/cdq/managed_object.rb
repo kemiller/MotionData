e5cf57bec47100f137704d37616a29b0014a396b
Ken Miller <ken@bitfield.net>
2013-07-06 14:29:27 -0700

    more comments

--- lib/cdq/managed_object.rb
+++ lib/cdq/managed_object.rb
@@ -1,32 +1,37 @@
 
 module CDQ
 
+  # CDQ Extensions for your custom entity objects.  This is mostly convenience
+  # and syntactic sugar -- you can access every feature using the cdq(Class).<em>method</em>
+  # syntax, but this enables the nicer-looking and more convenient Class.<em>method</em> style.
+  # Any method availble on cdq(Class) is now available directly on Class.
+  #
+  # If there is a conflict between a CDQ method and one of yours, or one of Core Data's,
+  # your code will always win.  In that case you can get at the CDQ method by calling
+  # Class.cdq.<em>method</em>.
+  #
+  # Examples:
+  # 
+  #   MyEntity.where(:name).eq("John").limit(2)
+  #   MyEntity.first
+  #   MyEntity.create(name: "John")
+  #   MyEntity.sort_by(:title)[4]
+  #
+  #   class MyEntity < CDQManagedObject
+  #     scope :last_week, where(:created_at).ge(date.delta(weeks: -2)).and.lt(date.delta(weeks: -1))
+  #   end
+  #
+  #   MyEntity.last_week.where(:created_by => john)
+  #
   class CDQManagedObject < NSManagedObject
 
     extend CDQ
 
     class << self
 
-      def where(*args)
-        cdq.where(*args)
-      end
-
-      def sort_by(*args)
-        cdq.sort_by(*args)
-      end
-
-      def limit(*args)
-        cdq.limit(*args)
-      end
-
-      def first
-        cdq.first
-      end
-
-      def all
-        cdq.all
-      end
-
+      # Creates a CDQ scope, but also defines a method on the class that returns the 
+      # query directly.
+      #
       def scope(name, query)
         cdq.scope name, query
         self.class.send(:define_method, name) do |*args|
@@ -34,6 +39,8 @@ module CDQ
         end
       end
 
+      # Pass any unknown methods on to cdq. 
+      #
       def method_missing(name, *args)
         cdq.send(name, *args)
       end
